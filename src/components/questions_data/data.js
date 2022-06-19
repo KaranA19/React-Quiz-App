a9857669b8371cec7bb36186b8603184edfa9d1f
Karan <57216123+KaranA19@users.noreply.github.com>
2022-06-19 12:40:26 +0530

    Add files via upload

---
+++ src/components/questions_data/data.js
@@ -0,0 +1,103 @@
+export default [
+    {
+        question: 'We can go for keys when there is possibility that our user could change the data?',
+        answers: [
+            'Keys',
+            'Ref',
+            'Both',
+            'None of above'
+        ],
+        correct: 1
+    },
+
+    {
+        question: 'JSX is typesafe?',
+        answers: [
+            'True',
+            'False',
+            'None of the Above',
+            'Cannot Be Determined'
+        ],
+        correct: 1
+    },    
+    {
+        question: 'React merges the object you provide into the current state using which of the following',
+        answers: [
+            'setState()',
+            'State()', 
+            'getState()', 
+            'None of the Above'
+        ],    
+        correct: 1
+    },    
+    {
+        question: 'Arbitrary inputs of components are called?',
+        answers: [
+            'Keys',
+            'Props', 
+            'Elements', 
+            'Ref'
+        ],    
+        correct: 2
+    },
+    {
+        question: '_____ can be done while more than one element needs to be returned from a component?',
+        answers: [
+            'Abstraction',
+            'Packing', 
+            'Insulation', 
+            'Wrapping'
+        ],    
+        correct: 4
+    },
+    {
+        question: 'Which of the following needs to be updated to achieve dynamic UI updates?',
+        answers: [
+            'State',
+            'Props', 
+            'Components', 
+            'None of the Above'
+        ],    
+        correct: 1
+    },
+    {
+        question: 'Lifecycle methods are mainly used _____',
+        answers: [
+            'To keep track of event history',
+            'To enhance components', 
+            'Free up resources', 
+            'None of the Above'
+        ],    
+        correct: 3
+    },
+    {
+        question: 'Which of the following keywords is used to define a variable in Javascript?',
+        answers: [
+            'Var',
+            'Let', 
+            'Both A and B', 
+            'None of the above'
+        ],    
+        correct: 3
+    },
+    {
+        question: 'Upon encountering empty statements, what does the Javascript Interpreter do?',
+        answers: [
+            'Throws an error',
+            'Ignores the statements', 
+            'Gives a Warning', 
+            'None of the above'
+        ],    
+        correct: 2
+    },
+    {
+        question: 'What keyword is used to check whether a given property is valid or not?',
+        answers: [
+            'is in',
+            'exists', 
+            'lies', 
+            'in'
+        ],    
+        correct: 4
+    }
+]
